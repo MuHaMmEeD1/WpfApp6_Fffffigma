67ea2699d9e9843bb6a756496c2df8f42bc1367f
MuHaMmEeD1 <139770024+MuHaMmEeD1@users.noreply.github.com>
2023-11-03 19:40:29 +0400

    Update Home.xaml.cs

--- WpfApp6_Figma/Pages/Home.xaml.cs
+++ WpfApp6_Figma/Pages/Home.xaml.cs
@@ -1,7 +1,9 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -22,6 +24,7 @@ namespace WpfApp6_Figma.Pages
     {
         public Home()
         {
+            
             InitializeComponent();
         }
 
